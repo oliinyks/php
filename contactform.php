4ac4e8abe9e46d9f2489ebd4f5b6e3dae967d47d
Olijnyks <101010916+oliinyks@users.noreply.github.com>
2023-01-27 17:48:26 +0200

    add form

---
+++ contactform.php
@@ -0,0 +1,14 @@
+
+// if(isset($_POST['submit'])){
+// 	$name = $_POST['name'];
+// 	$mailFrom = $_POST['mail'];
+// 	$subject = $_POST['subject'];
+// 	$message = $_POST['message'];
+
+// 	$mailTo = 'dddd@ukr.net';
+// 	$headers = "From: {$mailFrom}";
+// 	$txt = "You have received an e-mail from {$name}" . "\n\n" . $message;
+
+// 	mail($mailTo, $subject, $txt, $headers);
+// 	header("Location: index.php?mailsend");
+// }
---
+++ incs/data.php
@@ -0,0 +1,20 @@
+<?php
+
+$fields = [
+	'name' => [
+	'field_name' => 'Name',
+   'required' => 1,
+	],
+	'mail' => [
+	'field_name' => 'Mail',
+	'required' => 1,
+	],
+	'subject' => [
+	'field_name' => 'Subject',
+	'required' => 0,
+	],
+	'message' => [
+	'field_name' => 'Message',
+	'required' => 1,
+	],
+];
